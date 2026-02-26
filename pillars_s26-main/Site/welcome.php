c88b0a0f7207aaef233a40c69350a990619379f0
Pratyush <lc23-0429@lclark.edu>
2026-02-26 14:00:15 -0800

    Add files via upload

---
+++ pillars_s26-main/Site/welcome.php
@@ -0,0 +1,37 @@
+<!-- <?php
+// $servername = "localhost"; 
+// $username = "root";    
+// $password = "";           
+// $dbname = "RegisteredUser"; 
+
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// if (!$conn) {
+//   die("Connection failed: " . mysqli_connect_error());
+// }
+// // echo "Connected successfully";
+
+
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome To The Seven Pillars Game</title>
+  <link rel="stylesheet" href="welcome_style.css">
+</head>
+<body>
+  <img src="seven_img_asset.png" alt="comp logo" class = "top-left-image" width = "200" height = "100" >
+    <div class="container">
+    <div class="content">
+        <h1>Welcome To The Seven Pillars Game !</h1>
+          <a href="register.php" class="register"> Register</a>
+
+        <a href="guest.php" class="guest">Play as Guest</a>
+    </div>
+</div>
+
+</body>
+</html>
